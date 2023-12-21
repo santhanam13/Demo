c320ea447b10e4386245807e05348a92795441a7
Santhanam <154440698+Santta1@users.noreply.github.com>
2023-12-21 01:48:26 -0800

    Add cmmmit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/santhanam/Documents/GitHub/Demo/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/santhanam/Documents/GitHub/Demo/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/santhanam/Documents/GitHub/Demo/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/santhanam/Documents/GitHub/Demo/src/css/custom.css"),
+];
